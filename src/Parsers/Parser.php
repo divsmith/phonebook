8f5c7eada6fbc2bc26759eacb41ab4ee49868316
Parker Smith <parker@parkersmith.us>
2015-05-31 23:43:58 -0600

    Update docblock

--- src/Parsers/Parser.php
+++ src/Parsers/Parser.php
@@ -6,6 +6,7 @@ interface Parser {
      * Return the part of the URL used
      * for matching a tenant.
      *
+     * @param $url
      * @return string
      */
     public function parseUrl($url);
