3d274562512e457b1cba646a980eea16c01a529d
Parker Smith <parker@parkersmith.us>
2015-05-31 18:50:28 -0600

    Add phonebook middleware

---
+++ src/Phonebook.php
@@ -0,0 +1,35 @@
+<?php namespace Phonebook;
+
+use Closure;
+use Phonebook\Resolvers\TenantResolver;
+
+class Phonebook {
+
+    /**
+     * @var Phonebook\Resolvers\TenantResolver
+     */
+    protected $tenantResolver;
+
+    public function __construct(TenantResolver $tenantResolver)
+    {
+        $this->tenantResolver = $tenantResolver;
+    }
+
+    /**
+     * Get the tenant ID and put it into the request's
+     * attributes object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $tenantId = $this->tenantResolver->getTenantId();
+
+        $request->attributes->add('tenantId', $tenantId);
+
+        return $next($request);
+    }
+
+}
